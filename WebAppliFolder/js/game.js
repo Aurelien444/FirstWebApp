4224eb302549f4f0735d9c05275c26329bccca89
Goost <Paul.Snyk@gmail.com>
2021-01-11 10:28:20 +0100

    modif deplacement cube

--- WebAppliFolder/js/game.js
+++ WebAppliFolder/js/game.js
@@ -1,14 +1,15 @@
 const canvas = document.getElementById("canvasTest");
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'red';
-ctx.fillRect(10, 10, 100, 100);
-
-let frame = 0
+let x = 10
 
 function gameLoop(){
-    console.log(frame)
-    frame += 1
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 300, 300);
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(x, 10, 100, 100);
+    x += 0.5
 
 }
 
